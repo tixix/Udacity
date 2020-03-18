bd0c7f59e6ce227557f12cafe95657495094afb6
xhoa <send2me2nd@gmail.com>
2020-03-18 11:11:29 +0100

    first project finishes

--- 2.Data Modeling/Project_Postgres/sql_queries.py
+++ 2.Data Modeling/Project_Postgres/sql_queries.py
@@ -6,8 +6,6 @@ song_table_drop = "DROP TABLE IF EXISTS songs"
 artist_table_drop = "DROP TABLE IF EXISTS artists"
 time_table_drop = "DROP TABLE IF EXISTS time"
 
-print("DROP tables DONE.")
-print("CREATE TABLES...")
 # CREATE TABLES
 
 artist_table_create = ("""
@@ -19,7 +17,7 @@ CREATE TABLE IF NOT EXISTS artists (artist_id VARCHAR PRIMARY KEY ,
                                     );
 """)
 
-print("artists table created.")
+
 
 user_table_create = ("""
 CREATE TABLE IF NOT EXISTS users (user_id varchar PRIMARY KEY,
@@ -29,7 +27,7 @@ CREATE TABLE IF NOT EXISTS users (user_id varchar PRIMARY KEY,
                                       level VARCHAR  NOT NULL           
                                     );
 """)
-print("users table created.")
+
 
 song_table_create = ("""
 CREATE TABLE IF NOT EXISTS songs (song_id varchar PRIMARY KEY,
@@ -40,7 +38,7 @@ CREATE TABLE IF NOT EXISTS songs (song_id varchar PRIMARY KEY,
                                     );
 """)
 
-print("songs table created.")
+
 
 time_table_create = ("""
 CREATE TABLE IF NOT EXISTS time (start_time TIMESTAMP PRIMARY KEY,
@@ -55,7 +53,7 @@ CREATE TABLE IF NOT EXISTS time (start_time TIMESTAMP PRIMARY KEY,
 
 songplay_table_create = (""" 
 CREATE TABLE IF NOT EXISTS songplays (        
-                                    songplay_id INTEGER ,
+                                    songplay_id SERIAL PRIMARY KEY,
                                     start_time TIMESTAMP NOT NULL,
                                     user_id INTEGER NOT NULL,
                                     level VARCHAR,
@@ -66,41 +64,40 @@ CREATE TABLE IF NOT EXISTS songplays (
                                     user_agent VARCHAR
                                     );
 """)
-print("songplays table created.")
-print("INSERT RECORDS...")
+
 # INSERT RECORDS
 
-songplay_table_insert = ("""
-INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
-VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);
+songplay_table_insert = (""" 
+INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
+    VALUES ( %s, %s, %s, %s, %s, %s, %s, %s) 
 """)
-print("Inserted songplays RECORDS.")
+
 user_table_insert = ("""
 INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s,%s,%s,%s,%s) 
     ON CONFLICT (user_id)
     DO UPDATE
         SET level      = EXCLUDED.level
 """)
-print("Inserted users RECORDS.")
+
 song_table_insert = ("""
 INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s,%s,%s,%s,%s) 
 ON CONFLICT (song_id) 
 DO NOTHING;
 """)
-print("Inserted songs RECORDS.")
+
 artist_table_insert = ("""
 INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s,%s,%s,%s,%s) 
 ON CONFLICT (artist_id) 
 DO NOTHING;
 """)
-print("Inserted artists RECORDS.")
+
 
 time_table_insert = ("""
 INSERT INTO time (start_time , hour ,day , week , month ,year ,weekday ) VALUES (%s,%s,%s,%s,%s,%s,%s) 
 ON CONFLICT  (start_time) 
 DO NOTHING;
 """)
-print("Inserted time RECORDS.")
+
 # FIND SONGS
 song_select = ("""
 SELECT songs.song_id, artists.artist_id 
